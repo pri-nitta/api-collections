a6eda7eea240b9a2f25dc525ab22eb0bc10d691a
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-23 16:05:52 -0300

    array with double

---
+++ src/main/kotlin/colletcions/DoubleArray.kt
@@ -0,0 +1,29 @@
+package colletcions
+
+fun main() {
+    val salarios = DoubleArray(3)
+    salarios[0] = 4000.00
+    salarios[1] = 3500.00
+    salarios[2] = 6570.00
+
+    salarios.sort()
+    salarios.forEach {
+        println(it)
+    }
+
+    println("")
+
+    //já nos dá o indice
+    salarios.forEachIndexed {index, salario ->
+salarios[index] = salario * 1.1
+    }
+
+    salarios.forEach {println(it)}
+
+    println("")
+
+    //pode indicar os valores
+    val salarios2 = doubleArrayOf(5800.00, 9000.00,3700.00,15500.00)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+}
